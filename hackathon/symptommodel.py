261ed458ce8d56af37707d5b64799d4472a117f8
zereamirbekova <60564098+zereamirbekova@users.noreply.github.com>
2020-02-02 12:21:57 +0600

    Add files via upload

---
+++ hackathon/symptommodel.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+class SymptomModel():
+    def __init__(self, connection):
+        self.connection = connection
+        
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS symptoms 
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             symptom VARCHAR(50),
+                             illness VARCHAR(50),
+                             illness_id VARCHAR(50))''')
+        cursor.close()
+        self.connection.commit()
+     
+    def insert(self, symptom, illness, illness_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO symptoms 
+                          (symptom, illness, illness_id) 
+                          VALUES (?,?,?)''', (symptom, illness, illness_id))
+        cursor.close()
+        self.connection.commit()     
+        
+    def exists(self, symptom):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM symptoms WHERE symptom = ?", (symptom,))
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False,)
+    
+    def get(self, symptom_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM symptoms WHERE id = ?", (str(symptom_id),))
+        row = cursor.fetchone()
+        return row
+    
+    def get_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM symptoms")
+        rows = cursor.fetchall()
+        return rows    
